6a933d029f97f63726a4bad38012fd0ef82d1492
smmahmudulhasansiddiquei <mahmudcse03@gmail.com>
2025-12-24 01:04:21 +0600

    first commit

---
+++ calculator.java
@@ -0,0 +1,33 @@
+class Calculator {
+
+    int add(int a, int b) {
+        return a + b;
+    }
+
+    int subtract(int a, int b) {
+        return a - b;
+    }
+
+    int multiply(int a, int b) {
+        return a * b;
+    }
+
+    int divide(int a, int b) {
+        if (b == 0) {
+            System.out.println("Cannot divide by zero!");
+            return 0;
+        }
+        return a / b;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+
+        System.out.println("Add: " + calc.add(10, 5));
+        System.out.println("Subtract: " + calc.subtract(10, 5));
+        System.out.println("Multiply: " + calc.multiply(10, 5));
+        System.out.println("Divide: " + calc.divide(10, 2));
+    }
+}
